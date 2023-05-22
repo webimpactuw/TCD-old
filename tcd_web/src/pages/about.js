9dd65b3fec506cf8839fc8484ae77a9c628d55cf
Adventure293 <121738024+Adventure293@users.noreply.github.com>
2023-05-22 05:53:27 -0700

    Update about.js

--- tcd_web/src/pages/about.js
+++ tcd_web/src/pages/about.js
@@ -5,13 +5,15 @@ export default function About() {
         <div>
             <div className={classes.intro}>
                 <img src="../../public/about1.png"></img>
-                <h1>test</h1>
-                <h2>test</h2>
+                <div className={classes.introtext}>
+                    <h5>TCD @ UW</h5>
+                    <p>-- aims to spread Chinese culture in the greater Seattle area through events like performances and workshops</p>
+                </div>
             </div>
-            <div>           
-                <h2>test</h2>
-                <button>Interest Form</button>
-                <button>Remind</button>
+            <div className={classes.interest}>           
+                <p className={classes.contact}>To get in touch with us, email tcduw@uw.edu or check out our Interest Form and Remind</p>
+                <button className={classes.btn1}>Interest Form</button>
+                <button className={classes.btn2}>Remind</button>
             </div>
             <div className={classes.gallery}>
                 <h3>Gallery</h3>
