cc61379f23ec428b2a2261a4d186a1348cc04d85
Adventure293 <121738024+Adventure293@users.noreply.github.com>
2023-05-22 05:32:23 -0700

    Update about.js

--- tcd_web/src/pages/about.js
+++ tcd_web/src/pages/about.js
@@ -1,7 +1,27 @@
+import classes from './About.module.css';
+
 export default function About() {
     return (
         <div>
-
+            <div className={classes.intro}>
+                <img src="../../public/about1.png"></img>
+                <h1>test</h1>
+                <h2>test</h2>
+            </div>
+            <div>           
+                <h2>test</h2>
+                <button>Interest Form</button>
+                <button>Remind</button>
+            </div>
+            <div className={classes.gallery}>
+                <h3>Gallery</h3>
+                <p>test</p>
+                <img></img>
+                <img></img>
+                <img></img>
+                <img></img>
+                <img></img>
+            </div>
         </div>
     )
 }
